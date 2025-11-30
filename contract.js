a8ba4701da52d10be0529283b0a8eacba3b66ca5
ta1gos <153228770+ta1gos@users.noreply.github.com>
2025-11-30 13:52:12 +0300

    Create contract.js

---
+++ contract.js
@@ -0,0 +1,51 @@
+// Адрес вашего развернутого контракта
+const contractAddress = '0xbe1110872822299dDfCfcbB178ECb78B2d75caf8';
+// ABI вашего контракта (можно взять тот набор, который формирует Remix)
+const contractAbi = [
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_message",
+                "type": "string"
+            }
+        ],
+        "name": "setMessage",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getMessage",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+
+// При подключении к MetaMask
+if (window.ethereum) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+
+    document.getElementById('setMessageButton').onclick = async () => {
+        const message = document.getElementById('messageInput').value;
+        await contract.setMessage(message);
+        alert('Сообщение установлено!');
+    };
+
+    document.getElementById('getMessageButton').onclick = async () => {
+        const message = await contract.getMessage();
+        document.getElementById('messageDisplay').innerText = message;
+    };
+} else {
+    alert('Установите MetaMask или другой кошелек.');
+}
